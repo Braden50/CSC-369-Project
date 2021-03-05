2d952533444ed1dbcfba98fa1c175932ab87694d
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-03-04 17:55:15 -0800

    initial commit

---
+++ src/main/scala/App.scala
@@ -0,0 +1,16 @@
+import spire.compat.{numeric, ordering}
+
+import java.io.{File, PrintWriter}
+import scala.::
+import scala.collection.JavaConversions.seqAsJavaList
+import scala.collection.immutable
+import scala.collection.mutable
+import scala.io._
+
+
+object App {
+
+   def main(args: Array[String]): Unit = {
+      println("Hello world")
+   }
+}
